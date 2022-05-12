2205ca169408d29d6ce9d0c0939a8e63e5b21564
RajaSangeeth <60357139+RajaSangeeth@users.noreply.github.com>
2022-05-12 11:53:38 +0200

    commit

--- forest_plot_usage.R
+++ forest_plot_usage.R
@@ -4,7 +4,7 @@ library(ggthemes)
 
 ## Appliance Usage data frame
 data_plot_usage <- data.frame(
-  Appliance = c("Washing Machine", "Refrigerator", "Dishwasher",
+  Appliance = c("Washer/Dryer", "Refrigerator", "Dishwasher",
                 "Heat Pump", "Air Conditioning", "Lighting"),
   Effect_Size = c(-0.0001, 0.0040, -0.0007, 0.0009, 0.0, -0.0075),
   Lower = c(-0.0003, 0.0004, -0.0034, -0.0023, -0.0001, -0.0111),
@@ -17,9 +17,9 @@ ggplot(data_plot_usage, aes(Effect_Size, Appliance)) +
   geom_point(size = data_plot_usage$Number_of_effect_sizes) +
   geom_errorbarh(aes(xmin = Lower, xmax = Upper, height = .2)) +
   labs(x = "Effect Size") +
+  theme(axis.text = element_text(size = 20)) + 
   xlab("Pooled effect sizes across all interventions (r-Values)") +
-  ylab("Appliances") +
-  ggtitle("           Forest Plot of pooled effect sizes for the Usage of Appliances") +
+  ggtitle("                                   Plot of pooled effect sizes for the Usage of Appliances") +
   theme_economist()
 
 
